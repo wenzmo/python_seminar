3ccd483f1f486379fcf7744c2582520f3ac19818
wenzmo <wenzlerm@hu-berlin.de>
2018-06-19 16:46:12 +0200

    -

--- Session_9/assignemnt_8_wenzler.py
+++ Session_9/assignemnt_8_wenzler.py
@@ -19,12 +19,11 @@ outdir = "/home/mo/Dokumente/python_seminar/Session_9/outdata/"
 # ####################################### Functions ########################################################## #
 def TransformGeometry(geometry, target_sref):
     '''Returns cloned geometry, which is transformed to target spatial reference'''
-    geom_sref = geometry.GetSpatialReference()
+    geom_sref= geometry.GetSpatialReference()
     transform = osr.CoordinateTransformation(geom_sref, target_sref)
     geom_trans = geometry.Clone()
     geom_trans.Transform(transform)
     return geom_trans
-
 
 def SpatialReferenceFromRaster(ds):
     '''Returns SpatialReference from raster dataset'''
@@ -33,17 +32,21 @@ def SpatialReferenceFromRaster(ds):
     sr.ImportFromWkt(pr)
     return sr
 
-def CopySHPDisk(layer, outpath):
-    drvV = ogr.GetDriverByName('ESRI Shapefile')
-    outSHP = drvV.CreateDataSource(outpath)  # outpath
-    lyr = layer  # .GetLayer() #shape
-    sett90LYR = outSHP.CopyLayer(lyr, 'lyr')
-    del lyr, sett90LYR, outSHP
-# ####################################### PROCESSING ########################################################## #
+# read raster DEM
+dem = gdal.Open("DEM_Humboldt.tif")
+gt = dem.GetGeoTransform()
+pr = dem.GetProjection()
+sr_raster = SpatialReferenceFromRaster(dem)
 
-parcels = ogr.Open("Parcels.shp")
+driver = ogr.GetDriverByName('ESRI Shapefile')
+parcels = driver.Open("Parcels.shp")
 parcels_lyr = parcels.GetLayer()
 parcels_cs = parcels_lyr.GetSpatialRef()
+feat = parcels_lyr.GetNextFeature()
+
+mary = ogr.Open("Marihuana_Grows.shp")
+mary_lyr = mary.GetLayer()
+mary_cs = mary_lyr.GetSpatialRef()
 
 roads = ogr.Open("Roads.shp")
 roads_lyr = roads.GetLayer()
@@ -51,127 +54,101 @@ roads_lyr = roads.GetLayer()
 thp = ogr.Open("TimberHarvestPlan.shp")
 thp_lyr = thp.GetLayer()
 
-# Get Projection infos
-mary = ogr.Open("Marihuana_Grows.shp")
-mary_lyr = mary.GetLayer()
-mary_cs = mary_lyr.GetSpatialRef()
+public = ogr.Open("PublicLands.shp")
+public_lyr = public.GetLayer()
 
-dem = gdal.Open("DEM_Humboldt.tif")
-gt = dem.GetGeoTransform()
-pr = dem.GetProjection()
-sr_raster = SpatialReferenceFromRaster(dem)
-
-i = 0
-id = 0
-
+# Create output dataframe
 out_df = pd.DataFrame(columns=["Parcel APN", "NR_GH-Plants", "NR_OD-Plants", "Dist_to_grow_m", "Km Priv. Road", "Km Local Road", "Mean elevation", "PublicLand_YN", "Prop_in_THP"])
-
-feat = parcels_lyr.GetNextFeature()
 while feat:
-    geom = feat.GetGeometryRef()
     apn = feat.GetField('APN')
-    #
-    # ############################################################# #
-    #### Group 1 #####
+
+    ##### GROUP 1 #####
+    # count no. cannabis plants (greenhouse / open)
     geom_par = feat.geometry().Clone()
     geom_par.Transform(osr.CoordinateTransformation(parcels_cs, mary_cs))
-
     mary_lyr.SetSpatialFilter(geom_par)
-
     total_gh = total_od = 0
-
     point_feat = mary_lyr.GetNextFeature()
     while point_feat:
         total_gh += point_feat.GetField('g_plants')
         total_od += point_feat.GetField('o_plants')
         point_feat = mary_lyr.GetNextFeature()
-    mary_lyr.ResetReading()
-    #print(total_od,total_gh)
 
-    #### Group 2 #####
 
-    id += 1
-    parcel = geom_par.Clone()
-    distance = []
-    mary_lyr.SetSpatialFilter(parcel)
+    ##### GROUP 2 #####
+    # distance to next grow point
     feature_count = mary_lyr.GetFeatureCount()
-    print("ID: " + str(id) + " Feature Count: " + str(feature_count))
+    distance = 0
     bufferSize = 0
     if feature_count > 0:
         mary_lyr.SetSpatialFilter(None)
-
         exit = 0
         while exit == 0:
             bufferSize = bufferSize + 10
-
-            buffer = parcel.Buffer(bufferSize)
+            buffer = geom_par.Buffer(bufferSize)
             mary_lyr.SetSpatialFilter(buffer)
             buffer_count = mary_lyr.GetFeatureCount()
-            print("Current buffer size: " + str(bufferSize) + "Current buffer count:" + str(buffer_count))
-            # check if more marijuana plants in the buffer as in the parcel
             if buffer_count > feature_count:
                 exit += 1
+                distance = bufferSize
+    mary_lyr.ResetReading()
+    mary_lyr.SetSpatialFilter(None)
 
-        mary_lyr.SetSpatialFilter(None)
 
-    ######### Group 3 ############
-
+    ##### GROUP 3 #####
+    # roads in parcel
     # Set filter for relevant road types
     roads_lyr.SetAttributeFilter("FUNCTIONAL IN ('Local Roads', 'Private')")
     # loop through two categories
     road_feat = roads_lyr.GetNextFeature()
-    length_pr = length_lr = 0
+    geom_par = feat.geometry().Clone()
+    len_loc = len_priv = 0
     while road_feat:
         functional = road_feat.GetField('FUNCTIONAL')
         geom_roads = road_feat.GetGeometryRef()
-        intersection = geom.Intersection(geom_roads)  # calculate intersection of road types and individual parcel
+        intersection = geom_par.Intersection(geom_roads)  # calculate intersection of road types and individual parcel
         length = intersection.Length()  # get length of intersection
         if functional == 'Local Roads':
-            length_lr = round(length / 1000,3)
+            len_loc = length/1000
         if functional == 'Private':
-            length_pr = round(length / 1000,3)
+            len_priv = length/1000
         road_feat = roads_lyr.GetNextFeature()
-    roads_lyr.ResetReading()
 
-    area_parcel = geom.GetArea()
+
+    # ckeck for Timber Harvest Plan overlap
     # timber harvest plan > only use one year (overlapping geometries)
     thp_lyr.SetAttributeFilter("THP_YEAR = '1999'")
-    thp_lyr.SetSpatialFilter(geom)  # Set filter for parcel
+    thp_lyr.SetSpatialFilter(geom_par)  # Set filter for parcel
     thp_feat = thp_lyr.GetNextFeature()
-    area_parcel = geom.GetArea()  # area of parcel
+    area_parcel = geom_par.GetArea()  # area of parcel
     thp_list = []
-
     # loop through selected features
     while thp_feat:
         geom_thp = thp_feat.GetGeometryRef()
-        intersect_thp = geom.Intersection(geom_thp)  # intersection of parcel and selected thp features
+        intersect_thp = geom_par.Intersection(geom_thp)  # intersection of parcel and selected thp features
         area = intersect_thp.GetArea()  # area of intersected thp feature
         thp_list.append(area)  # add area of thp feature to list
         thp_feat = thp_lyr.GetNextFeature()
 
     thp_sum = sum(thp_list)  # sum up all thp features in parcel
     thp_prop = thp_sum / area_parcel
-    if thp_prop > 0:
-        thp_prop = int(1)
+    if thp_sum > 0:
+        thp_sum = int(1)
     else:
-        thp_prop = int(0)
+        thp_sum = int(0)
+    thp_lyr.SetSpatialFilter(None)
 
-    thp_sum = round(thp_sum,2)
-    ##### group 4 #####
+    ##### GROUP 4 #####
+    # check for public land in parcel
+    pl = 0
+    public_lyr.SetSpatialFilter(geom_par)
+    if public_lyr.GetFeatureCount() > 0:
+        pl = 1
+    public_lyr.SetSpatialFilter(None)
 
-
-
-
-
-
-    # read raster DEM
-
-    drv = ogr.GetDriverByName('ESRI Shapefile')
-
-    p_geom = TransformGeometry(geom_par, SpatialReferenceFromRaster(dem))
-
+    # mean elevation in parcel
     # Transform Coordinate System
-    p_geom_trans = TransformGeometry(p_geom, sr_raster)
+    p_geom_trans = TransformGeometry(geom_par, sr_raster)
     # Get Coordinates of polygon envelope
     x_min, x_max, y_min, y_max = p_geom_trans.GetEnvelope()
 
@@ -184,7 +161,6 @@ while feat:
     out_feat.SetGeometry(p_geom_trans)
     ds_lyr.CreateFeature(out_feat)
     out_feat = None
-    # CopySHPDisk(ds_lyr, "tryout.shp") #If you wish to check the shp
 
     # Create the destination data source
     x_res = math.ceil((x_max - x_min) / gt[1])
@@ -208,19 +184,16 @@ while feat:
     # Calculate the mean of the array with masking
     test_array = np.ma.masked_where(target_array < 1, target_array)
     raster_masked = np.ma.masked_array(raster_np, test_array.mask)
-    dem_mean = int(round(np.mean(raster_masked),0))
+    dem_mean = np.mean(raster_masked)
 
-
-
-    # ############################################################# #
-    #
-
-
-    out_df.loc[len(out_df) + 1] = [apn, total_gh, total_od, bufferSize,length_pr,length_lr,dem_mean,thp_prop,thp_sum]  # insert further variables from other groups
-
+    # append results in dataframe
+    out_df.loc[len(out_df) + 1] = [apn, total_gh, total_od, distance, len_priv, len_loc, dem_mean, thp_sum, thp_prop] # insert further variables from other groups
     feat = parcels_lyr.GetNextFeature()
+    #print("")
 
 parcels_lyr.ResetReading()
+
+# write dataframe to csv
 
 out_df.to_csv(outdir + "output_humboldt_county.csv", index=None, sep=',', mode='w')
 
